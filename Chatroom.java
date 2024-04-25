42afb72a2ffc1558a62cf4715eda88f8dd37de80
Derion12 <144293097+Derion12@users.noreply.github.com>
2024-04-25 01:12:27 -0700

    Update Chatroom.java

--- Chatroom.java
+++ Chatroom.java
@@ -1,4 +1,26 @@
 
 public class Chatroom {
+  private List<Message> messages;
+
+    // Constructor
+    public ChatRoom() {
+        messages = new ArrayList<>();
+    }
+
+    // Method to send a message to the chat room
+    public void sendMessage(String sender, String content) {
+        Message message = new Message(sender, content);
+        messages.add(message);
+    }
+
+    // Method to retrieve all messages in the chat room
+    public List<Message> getAllMessages() {
+        return messages;
+    }
+
+    // Method to clear all messages in the chat room
+    public void clearChat() {
+        messages.clear();
+    }
 
 }
