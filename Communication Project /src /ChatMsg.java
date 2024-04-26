3a92984c85e72a2b51b8db6f6b23b413bbe15109
Derion12 <144293097+Derion12@users.noreply.github.com>
2024-04-26 15:51:40 -0700

    Update ChatMsg.java

--- Communication Project /src /ChatMsg.java
+++ Communication Project /src /ChatMsg.java
@@ -1,4 +1,39 @@
+import java.util.Date;
 
 public class ChatMsg {
+    private int messageId;
+    private User sender;
+    private User receiver;
+    private String messageContent;
+    private Date timestamp;
 
+    public ChatMsg(int messageId, User sender, User receiver, String messageContent) {
+        this.messageId = messageId;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageContent = messageContent;
+        this.timestamp = new Date(); // Set current timestamp
+    }
+
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public String getMessageContent() {
+        return messageContent;
+    }
+
+    public Date getcurrentDate() {
+        return timestamp;
+    }
+
+   
 }
