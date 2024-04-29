9e66c83b1ad677b8f18ff033bef2c82576982c92
mesprisie <165091970+mesprisie@users.noreply.github.com>
2024-04-29 00:42:41 -0700

    Update Message.java

--- Communication Project /src /Message.java
+++ Communication Project /src /Message.java
@@ -2,31 +2,35 @@ import java.io.Serializable;
 
 public class Message implements Serializable {
 	protected static int count;
-    protected String type;
-    protected String status;
-    protected String text;
-    protected int id;
+  	protected MsgType type;
+   	protected MsgStatus status;
+    	protected String text;
+   	protected final int id;
 
     public Message(){
         this.type = "Undefined";
         this.status = "Undefined";
         this.text = "Undefined";
-        this.id = count++;
+        this.id = ++count;
     }
 
     public Message(String type, String status, String text){
         this.type = type;
         this.status = status;
         this.text = text;
+ 	this.id = ++count;
     }
 
     private void setType(String type){
+	    this.type = type
     }
 
     private void setStatus(String status){
+	    this.status = status;
     }
 
     private void setText(String text){
+	    this.text = text;
     }
 
     public int getID() {
