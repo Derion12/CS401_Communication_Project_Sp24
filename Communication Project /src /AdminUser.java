24e4b59361110874d5083f2c69f6da5ac1a5f126
mesprisie <165091970+mesprisie@users.noreply.github.com>
2024-04-29 13:51:33 -0700

    Delete Communication Project /src /AdminUser.java

--- Communication Project /src /AdminUser.java
+++
@@ -1,7 +0,0 @@
-public class AdminUser extends User {
-	public AdminUser(String userId, String username, String password) {
-        super(userId, username, password, UserStatus.Online);
-
-	}
-	
-}
