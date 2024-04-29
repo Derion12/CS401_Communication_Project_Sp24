2075c98089867dc05c7dc2bcb38130eb1bf299b6
mesprisie <165091970+mesprisie@users.noreply.github.com>
2024-04-29 14:36:48 -0700

    Update LogEntry.java

--- Communication Project /src /LogEntry.java
+++ Communication Project /src /LogEntry.java
@@ -1,35 +1,31 @@
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
-
-// Define a LogEntry class to represent individual log entries
-class LogEntry {
-    private String eventType;
-    private String description;
-    private Date timestamp;
-    // Constructor
-    public LogEntry(String timestamp, String eventType, String description) {
-        this.timestamp = timestamp;
-        this.eventType = eventType;
-        this.description = description;
-    }
-
-    // Getters
-    public LocalTime getTimestamp() {
-        return timestamp;
-    }
-
-    public String getEventType() {
-        return eventType;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    // toString method to represent LogEntry as a string
-    @Override
-    public String toString() {
-        return timestamp + " - " + eventType + ": " + description;
-    }
-}
+	// Define a LogEntry class to represent individual log entries
+	class LogEntry {
+	    private ChatMsg text;
+	    private Date timestamp;
+	    private Chatroom room;
+	    // Constructor
+	    public LogEntry(Date timestamp, ChatMsg text, Chatroom room) {
+	        this.timestamp = timestamp;
+	        this.text = text;
+	        this.room = room;
+	    }
+	
+	    // Getters
+	    public Date getTimestamp() {
+	        return timestamp;
+	    }
+	
+	    public ChatMsg getText() {
+	        return text;
+	    }
+	    
+	    public Chatroom getRoom() {
+	        return room;
+	    }
+	
+	    // toString method to represent LogEntry as a string
+	    @Override
+	    public String toString() {
+	        return timestamp + " - " + text.getSender() + ": " + text.getMessageContent();
+	    }
+	}
