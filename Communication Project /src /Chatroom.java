20445f112edf0ce7edb912a095c44f3b06e6ab22
mesprisie <165091970+mesprisie@users.noreply.github.com>
2024-04-29 14:33:16 -0700

    Update Chatroom.java

--- Communication Project /src /Chatroom.java
+++ Communication Project /src /Chatroom.java
@@ -1,26 +1,31 @@
+import java.util.ArrayList;
 
 public class Chatroom {
-  private List<Message> messages;
-
+  private ArrayList<LogList> logs;
+  private int id;
     // Constructor
-    public ChatRoom() {
-        messages = new ArrayList<>();
+    public Chatroom(ArrayList<LogList> roomLogs) {
+        logs = new ArrayList<LogList>();
+        logs = roomLogs;
     }
 
-    // Method to send a message to the chat room
-    public void sendMessage(String sender, String content) {
-        Message message = new Message(sender, content);
-        messages.add(message);
+    public int getID() {
+    	return id;
     }
-
-    // Method to retrieve all messages in the chat room
-    public List<Message> getAllMessages() {
-        return messages;
-    }
-
-    // Method to clear all messages in the chat room
-    public void clearChat() {
-        messages.clear();
-    }
+    
+//    // Method to send a message to the chat room
+//    public void sendMessage(String sender, String content) {
+//        
+//    }
+//
+//    // Method to retrieve all messages in the chat room
+//    public List<Message> getAllMessages() {
+//        return messages;
+//    }
+//
+//    // Method to clear all messages in the chat room
+//    public void clearChat() {
+//        messages.clear();
+//    }
 
 }
