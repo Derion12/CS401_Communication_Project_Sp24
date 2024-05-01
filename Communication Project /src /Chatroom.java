c6296ea96833b0e3176ded7bfb12fbe36040d21e
mesprisie <165091970+mesprisie@users.noreply.github.com>
2024-04-30 19:53:46 -0700

    Update Chatroom.java

--- Communication Project /src /Chatroom.java
+++ Communication Project /src /Chatroom.java
@@ -1,16 +1,33 @@
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Chatroom {
-  private ArrayList<LogList> logs;
-  private int id;
+public class Chatroom implements Serializable{
+  private ArrayList<LogEntry> logs;
+  private String name;
+  private int id = 0;
     // Constructor
-    public Chatroom(ArrayList<LogList> roomLogs) {
-        logs = new ArrayList<LogList>();
+    public Chatroom(ArrayList<LogEntry> roomLogs, String name, int id) {
+    	this.name = name;
+        logs = new ArrayList<LogEntry>();
         logs = roomLogs;
+        this.id = id;
     }
 
+    public String getName() {
+    	return name;
+    }
+    
     public int getID() {
     	return id;
+    }
+    
+    public ArrayList<String> printLogs(){
+    	ArrayList<String> logsToPrint = new ArrayList<String>();
+    	for(int i = 0; i<logs.size(); i++) {
+    		logsToPrint.add(logs.get(i).toString());
+    	}
+    	
+    	return logsToPrint;
     }
     
 //    // Method to send a message to the chat room
