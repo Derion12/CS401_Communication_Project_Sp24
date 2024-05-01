f4b9e4b94e96a81deca2b95fdefa8b7d21e46eb0
mesprisie <165091970+mesprisie@users.noreply.github.com>
2024-04-30 19:55:08 -0700

    Update Message.java

--- Communication Project /src /Message.java
+++ Communication Project /src /Message.java
@@ -1,11 +1,13 @@
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Message implements Serializable {
-	protected static int count;
+	protected static int count = 0;
   	protected MsgType type;
    	protected MsgStatus status;
     protected String text;
     protected boolean online;
+    protected ArrayList<Chatroom> rooms;
     
    	protected final int id;
 
@@ -13,13 +15,15 @@ public class Message implements Serializable {
         this.type = MsgType.UNDEFINED;
         this.status = MsgStatus.UNDEFINED;
         this.text = null;
+        this.rooms = null;
         this.id = ++count;
     }
 
-    public Message(MsgType type, MsgStatus status, String text){
+    public Message(MsgType type, MsgStatus status, String text, ArrayList<Chatroom> rooms){
         this.type = type;
         this.status = status;
         this.text = text;
+        this.rooms = rooms;
         this.id = ++count;
     }
 
@@ -35,6 +39,10 @@ public class Message implements Serializable {
 	    this.text = text;
     }
 
+    public ArrayList<Chatroom> getRooms(){
+	    return rooms;
+    }
+    
     public int getID() {
     	return id;
     }
